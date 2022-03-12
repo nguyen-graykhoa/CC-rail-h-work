1332034fc4b5eb0a598a65271293f1b1bce30ac6
khoa nguyen-gray <khoanguyengray@gmail.com>
2022-03-12 11:45:25 -0800

    post model initial create

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_185456) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_193356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
