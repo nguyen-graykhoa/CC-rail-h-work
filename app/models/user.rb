75e87815fb7ef58980fe2356a232d546adbe589f
khoa nguyen-gray <khoanguyengray@gmail.com>
2022-03-12 12:31:00 -0800

    setup 1 to many relationship for users posts comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
 
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    
     def full_name
         self.first_name + " " + self.last_name
     end
